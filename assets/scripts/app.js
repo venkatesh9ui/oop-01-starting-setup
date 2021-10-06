ed1e16911e08dc3357f2c6925ce0dc47b2f05fd4
venkatesh <venkatesh9ui@gmail.com>
2021-10-06 16:33:23 -0400

    missed this.render()

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -88,6 +88,7 @@ class ProductItem extends Component {
   constructor(product, renderHookId) {
     super(renderHookId, false);
     this.product = product;
+    this.render();
   }
 
   addToCart() {
